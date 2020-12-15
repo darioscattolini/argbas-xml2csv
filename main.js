225327737925e55160c9fa7562df4b22990f7ee1
Darío Scattolini <dario.scattolini@gmail.com>
2020-12-15 20:17:17 +0100

    abstract away more efficient product fields updater

--- main.js
+++ main.js
@@ -26,16 +26,31 @@ const psCombinationsFields = [
   'shop id/name', 'advanced stock managment', 'depends on stock', 'warehouse'
 ];
 
-const actionButton = document.querySelector('#convert');
+const updateProductsBtn = document.querySelector('#updateProducts');
 
-actionButton.addEventListener('click', async (event) => {
+updateProductsBtn.addEventListener('click', async (event) => {
   event.stopPropagation();
   event.preventDefault();
-  const option = document.querySelector('#options').value;
-  const xml = document.querySelector('#xml').files[0];
-  let csv;
+  
+  const psRawCsv = document.querySelector('#psProducts').files[0];
+  const teinorRawXml = document.querySelector('#xmlProducts').files[0];
+  
+  const psJson = await getPsProductsJson(psRawCsv);
+  const teinorXml = await getTeinorProductsXml(teinorRawXml);
+  const teinorJson = parseTeinorProducts(teinorXml);
+  
+  const updatedPsList = updatePsProducts(psJson, teinorJson);
+  const updatedProducts = updatedPsList.updatedProducts;
+  const unfoundProducts = updatedPsList.unfoundProducts;
 
-  switch (option) {
+  const headers = ['id', 'name', 'reference', 'priceTin', 'quantity', 'active'];
+  const updatedProductsCsv = buildCsv(updatedProducts, headers);
+  const unfoundProductsCsv = buildCsv(unfoundProducts, headers);
+
+  downloadCsv(updatedProductsCsv, 'updated_products.csv');
+  downloadCsv(unfoundProductsCsv, 'unfound_products.csv');
+
+  /*switch (option) {
     case "products":
       csv = await getProductsCsv(xml);
       for (const lang in csv) {
@@ -50,27 +65,135 @@ actionButton.addEventListener('click', async (event) => {
       const encodedUri = encodeURI(csv);
       downloadCsv(encodedUri, 'combinations.csv');
       break;
-  }
+  }*/
 });
 
-function downloadCsv(csvUri, fileName) {
+async function getPsProductsJson(psCsv) {
+  const csvContent = await getFileContent(psCsv);
+  const json = await csv({
+    delimiter: ';',
+    headers: [
+      'id', 'imageUrl', 'name', 'reference', 'category', 'priceTex', 'priceTin', 
+      'quantity', 'active', 'position'
+    ]
+  }).fromString(csvContent);
+  return json;
+}
+
+async function getTeinorProductsXml(rawXml) {
+  const parsedXml = await parseXml(rawXml);
+  const xmlProducts = Array.from(parsedXml.querySelectorAll('twinPrestaShop5'));
+  return xmlProducts;
+}
+
+function parseTeinorProducts(teinorProductsXml) {
+  const unifiedProducts = teinorProductsXml.reduce((unified, product) => {
+    const reference = product.querySelector('reference').innerHTML;
+    const price = product.querySelector('price_tin').innerHTML;
+    const quantity = product.querySelector('quantity').innerHTML;
+
+    if (unified.hasOwnProperty(reference)) {
+      unifiedProduct = unified[reference];
+
+      if (unifiedProduct.price !== price) {
+        unifiedProduct.consistentData = false;
+        unifiedProduct.price = String(Math.max(
+          Number(price), Number(unifiedProduct.price)
+        ));
+      }
+
+      if (unifiedProduct.quantity !== quantity) {
+        unifiedProduct.consistentData = false;
+        unifiedProduct.quantity = String(Math.min(
+          Number(quantity), Number(unifiedProduct.quantity)
+        ));
+        unifiedProduct.active = Number(unifiedProduct.quantity) > 3 ? '1' : '0';
+      }
+    } else {
+      unified[reference] = {};
+      unifiedProduct = unified[reference];
+      
+      unifiedProduct.price = price;
+      unifiedProduct.quantity = quantity;
+      unifiedProduct.active = Number(unifiedProduct.quantity) > 3 ? '1' : '0';
+      unifiedProduct.consistentData = true;
+    }
+
+    return unified;
+  }, {});
+
+  const products = [];
+  
+  for (const reference in unifiedProducts) {
+    const product = unifiedProducts[reference];
+    product.reference = reference;
+    products.push(product);
+  }
+
+  return products;
+}
+
+function updatePsProducts(psProducts, teinorProducts) {
+  const updatedProducts = [];
+  const unfoundProducts = [];
+
+  for (let product of psProducts) {
+    let updated = false;
+    product = Object.assign({}, product);
+    const teinorProduct = teinorProducts.find(x => x.reference === product.reference);
+    
+    if (!teinorProduct) {
+      unfoundProducts.push(product);
+      continue;
+    }
+
+    if (product.priceTin !== teinorProduct.price) {
+      updated = true;
+      product.priceTin = teinorProduct.price;
+    }
+
+    if (product.quantity !== teinorProduct.quantity) {
+      updated = true;
+      product.quantity = teinorProduct.quantity;
+    }
+
+    if (product.active !== teinorProduct.active) {
+      updated = true;
+      product.active = teinorProduct.active;
+    }
+
+    if (updated) updatedProducts.push(product);
+  }
+  
+  return { updatedProducts, unfoundProducts };
+}
+
+function buildCsv(json, headers) {
+  let csv = 'data:text/csv;charset=utf-8,';
+  csv += headers.join(';') + '\n';
+  json.forEach(item => { csv += buildCsvLine(item, headers); });
+  return csv;
+}
+
+function buildCsvLine(item, headers) {
+  let line = '';
+  for (const header of headers) {
+    let fieldValue = item[header];
+    fieldValue = fieldValue.replace(/"/g, '""');
+    line += `"${fieldValue}"`;
+    line += ';'
+  }
+  line += '\n';
+  return line;
+}
+
+function downloadCsv(csv, fileName) {
+  const csvUri = encodeURI(csv);
   const link = document.createElement("a");
   link.setAttribute("href", csvUri);
   link.setAttribute("download", fileName);
   document.body.appendChild(link);
   link.click();
-}
-
-async function getProductsCsv(xml) {
-  const xmlProducts = await parseXml(xml);
-  const products = Array.from(xmlProducts.querySelectorAll('twinPrestaShop5'));
-  const filteredByLang = filterByLang(products);
-  const csv = {};
-  for (const lang in filteredByLang) {
-    csv[lang] = parseProductsXml2Csv(filteredByLang[lang]);
-  }
-
-  return csv;
 }
 
 function filterByLang(xmlItems) {
@@ -93,7 +216,7 @@ function buildProductLine(xmlProduct) {
   let line = '';
   for (const field of psProductFields) {
     let fieldValue = getProductValue(xmlProduct, field);
-    fieldValue = fieldValue.replace(/"/g, "'");
+    fieldValue = fieldValue.replace(/"/g, '""');
     line += `"${fieldValue}"`;
     line += ';'
   }
@@ -138,8 +261,8 @@ function getProductValue(xmlProduct, field) {
       return xmlProduct.querySelector('weight').innerHTML;
     case 'quantity':
       return xmlProduct.querySelector('quantity').innerHTML;
-    case 'description':
-      return xmlProduct.querySelector('description_short').innerHTML;
+    /*case 'description':
+      return xmlProduct.querySelector('description_short').innerHTML;*/
     case 'meta title':
       return xmlProduct.querySelector('meta_title').innerHTML;
     case 'meta keywords':
@@ -195,7 +318,7 @@ function buildCombinationLine(xmlCombination) {
   let line = '';
   for (const field of psCombinationsFields) {
     let fieldValue = getCombinationValue(xmlCombination, field);
-    fieldValue = fieldValue.replace(/"/g, "'");
+    fieldValue = fieldValue.replace(/"/g, '""');
     line += `"${fieldValue}"`;
     line += ';'
   }
